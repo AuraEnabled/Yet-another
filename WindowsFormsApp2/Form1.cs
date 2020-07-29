5e4c0ba55e95ffb5fe02ece96adfb8e137bd7e3d
Борис Бритва <boris.gribanov@gmail.com>
2020-07-29 23:05:00 +0300

    Initial commit

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Click += async (s, a) =>
+            { 
+                await Task.Delay(1000); 
+                Process.Start("notepad");
+            };
+            button1.MouseEnter += (s, a) => { Process.Start("notepad"); };
+            button1.MouseLeave += (s, a) => { foreach (Process process in Process.GetProcessesByName("notepad")) process.Kill(); };
+        }
+
+        void Cmd(string line)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = $"/c {line}",
+                WindowStyle = ProcessWindowStyle.Hidden
+            }).WaitForExit(); 
+        }
+
+        
+    }
+}
